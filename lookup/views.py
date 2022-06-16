f6fc3e2748cd59270f631161507d3cd4ead2da24
hamlet07 <hamlet07@o2.pl>
2022-06-16 12:08:35 +0200

    Different cities

--- lookup/views.py
+++ lookup/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+from requests.api import post
 
 def home(request):
     import json
     import requests
 
-    api_request = requests.get("http://dataservice.accuweather.com/currentconditions/v1/276594?apikey=l4sSCFpiMOGuYpdVWHH4yXmFauJGy2rs&language=pl-pl&details=True")
+    city_code = '276594'
+    city = 'Poznań'
+
+    if request.method == "POST":
+        city = request.POST.get('city')
+        if city == 'Poznań':
+            city_code = '276594'
+        elif city == 'Gdańsk':
+            city_code = '275174'
+        elif city == 'Kraków':
+            city_code = '274455'
+        elif city == 'Szczecin':
+            city_code = '276655'
+        elif city == 'Warszawa':
+            city_code = '274663'
+        elif city == 'Wrocław':
+            city_code = '273125'
+        else: city_code = '276594'
+        #return render(request, 'home.html', {'city': city, 'city_code': city_code})
+    
+    api_request = requests.get("http://dataservice.accuweather.com/currentconditions/v1/" + city_code + "?apikey=l4sSCFpiMOGuYpdVWHH4yXmFauJGy2rs&language=pl-pl&details=True")
 
     try:
         api = json.loads(api_request.content)
@@ -30,9 +51,9 @@ def home(request):
     elif (api[0]['WeatherIcon'] >= 39)&(api[0]['WeatherIcon'] <=44):
         category_color = 'cloudy'
     else: category_color = 'neutral'
-    
+        
     return render(request, 'home.html', {'api': api, 
-                    'category_color': category_color})
+                        'category_color': category_color, 'city': city, 'city_code': city_code})
 
 def about(request):
     return render(request, 'about.html', {})
